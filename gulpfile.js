9b0de68073f64cd4fab9fa5a877a075165346a21
Vasiliy Susoev <susoev.vasil@mail.ru>
2020-04-21 00:59:56 +0300

    продолжение редактирования файла gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const spritesmith = require('gulp.spritesmith');
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            port: 9000,
+            baseDir: "build"
+        }
+    });
+
+
+    gulp.watch('build/**/*').on('change', browserSync.reload);
+});
